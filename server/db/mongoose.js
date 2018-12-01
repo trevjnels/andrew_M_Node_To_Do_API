6b0f64c9194b70f349794fbe1f2d9b41b0f8be42
trevjnels <trevorjohnnels@gmail.com>
2018-12-01 17:17:12 -0500

    fixed mongoose for conecting to MOGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-mongoose.connect(process.env.MONGDB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+);
 
 module.exports = { mongoose };
+
+//heroku logs shows you server logs for the application
