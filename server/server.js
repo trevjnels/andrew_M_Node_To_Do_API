1e09707aaff513a1aacc27f117176a4fcae52eda
trevjnels <trevorjohnnels@gmail.com>
2018-12-01 20:43:47 -0500

    added DELETE /todos testing

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.delete(`/todos/:id`, (req, res) => {
       if (!todo) {
         return res.status(404).send();
       }
-      res.send(todo);
+      res.send({ todo });
     })
     .catch(e => {
       res.status(400).send();
