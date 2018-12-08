92d3d63782c49d1dc4657116cbc1c5f729b45070
trevjnels <trevorjohnnels@gmail.com>
2018-12-08 08:19:23 -0500

    set up config file with test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,15 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
-);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
 
-//heroku logs shows you server logs for the application
+// //heroku logs shows you server logs for the application
+//  process.env.NODE_ENV === 'production'
+//  //heroku
+//  process.env.NODE_ENV === 'test'
+//  //mocha
+//  process.env.NODE_ENV === 'development'
+//  //localhost:3000
